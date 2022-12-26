67baf1835efd557a0282a40d5f715f32acfa3e28
Bhushan727 <bhushans727@gmail.com>
2022-12-26 15:42:51 +0530

    deleted new folder

--- New folder/src/state/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-// import { combineReducers } from 'redux';
-import amountReducers from './amountReducer';
-
-const reducers = combineReducers({
-    amount : amountReducers
-})
-
-export default reducers;
